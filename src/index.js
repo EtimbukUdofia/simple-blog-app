074fc7f1e5b39eb2dbd05210073a92640b2c135d
EtimbukUdofia <etimbukudofia9@gmail.com>
2023-09-30 16:05:21 +0100

    Add page routing

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <Route path="/" Component={App} />
+      <App/>
     </Router>
   </React.StrictMode>
 );
