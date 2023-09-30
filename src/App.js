1cdb4ede4f144025f787f6a40f54bb3b74a729ec
EtimbukUdofia <etimbukudofia9@gmail.com>
2023-09-30 09:26:57 +0100

    Add component files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-import './App.css';
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
 
 function App() {
   return (
